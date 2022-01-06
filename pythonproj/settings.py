4d54d21c07c32f54996e27ce9fe1b78b9e0071a0
Sagnikpal <74464019+sagnik2001@users.noreply.github.com>
2022-01-06 13:55:51 +0530

    First Commit

--- pythonproj/settings.py
+++ pythonproj/settings.py
@@ -126,7 +126,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 LOGIN_URL = 'login'
 
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
